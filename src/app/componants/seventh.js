05b98b55b114d086ea941bc80d32953c10a243a3
Zubair <zbrlohar@gmail.com>
2024-11-17 15:51:29 +0500

    Update seventh.js

--- src/app/componants/seventh.js
+++ src/app/componants/seventh.js
@@ -4,7 +4,7 @@ export default function footer() {
   return (
     <div className="grid bg-white pb-8 place-items-center font-custom tracking-widest">
       <div className=" mt-5 text-xl text-[#0079ac]">
-        © 2024 by Great Gonzo. All rights reserved!
+        © 2024 by Great Gonzo. All rights reserved
       </div>
     </div>
   );
